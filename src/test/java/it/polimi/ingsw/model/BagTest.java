2fbfe0d77f91e65d289bed3d2818eb0a46c4fc4d
vcristiano2000 <cristiano.valtolina@mail.polimi.it>
2023-03-16 14:38:34 +0100

    change bag's method name from pop to drawCard and create Bagtest

---
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class BagTest extends TestCase {
+
+
+    @Test
+    void drawCard() {
+    }
+}
