840f5ed947957328097c0b79d3becb0a1014a596
DiegosDigos <diego3.vigano@mail.polimi.it>
2023-03-16 11:32:52 +0100

    updated datGenerator
    all board schemes

--- src/main/resources/datGenerator.java
+++ src/main/resources/datGenerator.java
@@ -1,5 +1,18 @@
+import java.io.*;
 public class datGenerator {
     public void main() {
+
+        int[][] board2 = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
 
         int[][] board3 = {
                 {0, 0, 0, 1, 0, 0, 0, 0, 0},
@@ -10,10 +23,9 @@ public class datGenerator {
                 {1, 1, 1, 1, 1, 1, 1, 0, 0},
                 {0, 0, 1, 1, 1, 1, 1, 0, 0},
                 {0, 0, 0, 0, 1, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0, 0},};
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board3.dat"))) {
-            outputStream.writeObject(board3);
-        }
+                {0, 0, 0, 0, 0, 1, 0, 0, 0},
+        };
+
         int[][] board4 = {
                 {0, 0, 0, 1, 1, 0, 0, 0, 0},
                 {0, 0, 0, 1, 1, 1, 0, 0, 0},
@@ -23,7 +35,15 @@ public class datGenerator {
                 {1, 1, 1, 1, 1, 1, 1, 1, 0},
                 {0, 0, 1, 1, 1, 1, 1, 0, 0},
                 {0, 0, 0, 1, 1, 1, 0, 0, 0},
-                {0, 0, 0, 0, 1, 1, 0, 0, 0},};
+                {0, 0, 0, 0, 1, 1, 0, 0, 0},
+        };
+
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board2.dat"))) {
+            outputStream.writeObject(board2);
+
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board3.dat"))) {
+            outputStream.writeObject(board3);
+
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board4.dat"))) {
             outputStream.writeObject(board4);
             
