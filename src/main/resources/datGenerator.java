5318517e45fdeab1e1d246c1d398a67d774d1f9c
DiegosDigos <diego3.vigano@mail.polimi.it>
2023-03-16 12:30:54 +0100

    debugged datGenerator

--- src/main/resources/datGenerator.java
+++ src/main/resources/datGenerator.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class datGenerator {
-    public void main() {
+    public static void main(String args[]) {
 
         int[][] board2 = {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -38,15 +38,19 @@ public class datGenerator {
                 {0, 0, 0, 0, 1, 1, 0, 0, 0},
         };
 
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board2.dat"))) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board2.dat"));
             outputStream.writeObject(board2);
+        } catch (Exception e) {}
 
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board3.dat"))) {
-            outputStream.writeObject(board3);
+        // try (outputStream = new ObjectOutputStream(new FileOutputStream("board3.dat"))) {
+        //     outputStream.writeObject(board3);
+        // }
+        //
+        // try (outputStream = new ObjectOutputStream(new FileOutputStream("board4.dat"))) {
+        //     outputStream.writeObject(board4);
+        // }
 
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board4.dat"))) {
-            outputStream.writeObject(board4);
-            
-        }
+
     }
 }
