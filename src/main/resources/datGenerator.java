8e84485fa169dd9823d5b38a7d34c58bf0794518
vcristiano2000 <cristiano.valtolina@mail.polimi.it>
2023-03-16 11:25:10 +0100

    Update datGenerator.java

--- src/main/resources/datGenerator.java
+++ src/main/resources/datGenerator.java
@@ -1,5 +1,31 @@
 public class datGenerator {
-    public void main(){
-        
+    public void main() {
+
+        int[][] board3 = {
+                {0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0},};
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board3.dat"))) {
+            outputStream.writeObject(board3);
+        }
+        int[][] board4 = {
+                {0, 0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 0, 0, 0},};
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("board4.dat"))) {
+            outputStream.writeObject(board4);
+        }
     }
 }
