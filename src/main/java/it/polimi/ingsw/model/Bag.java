fe3ab134dff4757193899dad98f5f740eb5d69c5
vcristiano2000 <126269576+vcristiano2000@users.noreply.github.com>
2023-03-18 15:50:54 +0100

    implemented all the bag methods and created the bag test which specifically checks the contents of the bag after the first extraction and when the bag is empty

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -1,17 +1,40 @@
 package it.polimi.ingsw.model;
-
-import java.util.Set;
+import java.util.*;
 
 public class Bag extends GameObject {
     private Set<ItemCard> cardsInside;
-    private int maxCards;
+    private int maxCards=132;
 
     public Bag() {
-        throw new UnsupportedOperationException("TO DO");
-        // need ItemCard constructor to define the method
+        cardsInside = new HashSet<ItemCard>();
+        for(int i =0; i<maxCards ;i+=6){
+            ItemCard ic;
+            cardsInside.add(ic = new ItemCard(ItemType.BOOKS));
+            cardsInside.add(ic = new ItemCard(ItemType.GAMES));
+            cardsInside.add(ic = new ItemCard(ItemType.PLANTS));
+            cardsInside.add(ic = new ItemCard(ItemType.TROPHIES));
+            cardsInside.add(ic = new ItemCard(ItemType.FRAMES));
+            cardsInside.add(ic = new ItemCard(ItemType. CATS));
+
+        }
     }
 
-    public PersonalGoalCard drawCard() {
-        throw new UnsupportedOperationException("TO DO");
+    public Set<ItemCard> getCardsInside() {
+        return cardsInside;
+    }
+
+    public ItemCard drawCard(Set<ItemCard> cardsInside) {
+        if(cardsInside.size()>0){
+            ItemCard ic_2;
+            ic_2= getRandomSetElement(cardsInside);
+            cardsInside.remove(ic_2);
+            return ic_2;
+        }
+        else return null;
+
+    }
+
+    public static <Itemcard> Itemcard getRandomSetElement(Set<Itemcard> set) {
+        return set.stream().skip(new Random().nextInt(set.size())).findFirst().orElse(null);
     }
 }
