5500fcb4a6902675f919457dc963ed9f4fe590d5
vcristiano2000 <cristiano.valtolina@mail.polimi.it>
2023-04-13 10:39:54 +0200

    merged the common goal card FOUR_LINES_MAX_THREE_TYPES and THREE_COLUMNS_MAX_THREE_TYPES into MAX_THREE_TYPES

---
+++ src/main/java/it/polimi/ingsw/model/CommonGoalCardStrat_MAX_THREE_TYPES.java
@@ -0,0 +1,95 @@
+package it.polimi.ingsw.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static it.polimi.ingsw.utils.Constants.numberOfColumns;
+import static it.polimi.ingsw.utils.Constants.numberOfRows;
+
+public class CommonGoalCardStrat_MAX_THREE_TYPES implements CommonGoalCardStrat {
+
+    private final int numberOfColumnsToCheck;
+
+    private final int numberOfLinesToCheck;
+    private final int maxNumberOfTypes;
+    private final CommonGoalCardType type;
+
+    public CommonGoalCardStrat_MAX_THREE_TYPES(CommonGoalCardType cardType) throws RuntimeException {
+        type = cardType;
+        maxNumberOfTypes = 3;
+        if (cardType.equals(CommonGoalCardType.FOUR_LINES_MAX_THREE_TYPES)) {
+            numberOfColumnsToCheck = 0;
+            numberOfLinesToCheck = 4;
+
+        } else if (cardType.equals(CommonGoalCardType.THREE_COLUMNS_MAX_THREE_TYPES)) {
+            numberOfColumnsToCheck = 3;
+            numberOfLinesToCheck = 0;
+
+        } else throw new RuntimeException("WRONG COMMONCARD TYPE");
+    }
+
+    public boolean checkPattern(Shelf shelf) {
+        int counterOfLines = 0;
+        int counterOfColumns = 0;
+        // counts the number of rows that have a maximum of three different types of cards
+
+        outer:
+        for (int row = 0; row < numberOfRows && counterOfLines < numberOfLinesToCheck; row++) {
+
+            // if any position of the row is empty, is impossible to satisfy the required pattern,
+            // so skips to the next row
+            for (int column = 0; column < numberOfColumns; column++) {
+                if (shelf.getCardAt(row, column) == null) {
+                    continue outer;
+                }
+            }
+
+            Set<ItemType> typesInCurrentRow = new HashSet<>();
+            for (int column = 0; column < numberOfColumns; column++) {
+                ItemCard currentCard = shelf.getCardAt(row, column);
+
+                // if there's no card in that position continue.
+                // for security reasons
+                if (currentCard == null) continue outer;
+
+                // if a card has a different type from the one already in the set and the set reached
+                // the maximum number of possible types skips that row
+                if (!typesInCurrentRow.contains(currentCard.type()) && typesInCurrentRow.size() == maxNumberOfTypes)
+                    continue outer;
+                typesInCurrentRow.add(currentCard.type());
+            }
+            // if it reaches the end of the row, that row satisfies the required pattern
+            counterOfLines++;
+        }
+
+        outer1:
+        for (int column = 0; column < numberOfColumns && counterOfColumns < numberOfColumnsToCheck; column++) {
+            if (shelf.getCardAt(0, column) == null) continue;
+
+            Set<ItemType> typesInCurrentColumn = new HashSet<>();
+            for (int row = 0; row < numberOfRows; row++) {
+                ItemCard currentCard = shelf.getCardAt(row, column);
+
+                // if there's no card in that position continue.
+                // for security reasons
+                if (currentCard == null) continue;
+
+                // if a card has a different type from the one already in the set and the set reached
+                // the maximum number of possible types skips that row
+                if (!typesInCurrentColumn.contains(currentCard.type()) && typesInCurrentColumn.size() == maxNumberOfTypes)
+                    continue outer1;
+                typesInCurrentColumn.add(currentCard.type());
+            }
+            // if it reaches the end of the column, that column satisfies the required pattern
+            counterOfColumns++;
+        }
+
+        return (counterOfLines >= numberOfLinesToCheck && counterOfColumns >= numberOfColumnsToCheck);
+
+    }
+
+
+    public CommonGoalCardType getType() {
+        return type;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/CommonGoalCardStrat_MAX_THREE_TYPESTest.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CommonGoalCardStrat_MAX_THREE_TYPESTest {
+    String folderName1 = "/shelvesForTesting/testForFOUR_LINES_MAX_THREE_TYPES/";
+    String folderName = "/shelvesForTesting/testForTHREE_COLUMNS_MAX_THREE_TYPES/";
+    String fileExtension = ".txt";
+    int numberOfTests = 10;
+
+    @Test
+    void testCheckPatternMethod() {
+        CommonGoalCardStrat_MAX_THREE_TYPES commonGoalCardStrat = new CommonGoalCardStrat_MAX_THREE_TYPES(CommonGoalCardType.THREE_COLUMNS_MAX_THREE_TYPES);
+
+        for (int i = 0; i < numberOfTests; i++) {
+            String fileName;
+            if (i + 1 < 10) fileName = String.format(folderName + "/TEST_FALSE_0%d" + fileExtension, i + 1);
+            else fileName = String.format(folderName + "/TEST_FALSE_%d" + fileExtension, i + 1);
+
+            Shelf shelf = new Shelf(fileName);
+
+            assertFalse(commonGoalCardStrat.checkPattern(shelf));
+        }
+
+        for (int i = 0; i < numberOfTests; i++) {
+            String fileName;
+            if (i + 1 < 10) fileName = String.format(folderName + "TEST_TRUE_0%d" + fileExtension, i + 1);
+            else fileName = String.format(folderName + "TEST_TRUE_%d" + fileExtension, i + 1);
+
+            Shelf shelf = new Shelf(fileName);
+
+            assertTrue(commonGoalCardStrat.checkPattern(shelf));
+        }
+    }
+
+
+    @Test
+    void testCheckPatternMethod1() {
+        CommonGoalCardStrat_MAX_THREE_TYPES commonGoalCardStrat = new CommonGoalCardStrat_MAX_THREE_TYPES(CommonGoalCardType.FOUR_LINES_MAX_THREE_TYPES);
+
+        for (int i = 0; i < numberOfTests; i++) {
+            String fileName;
+            if (i + 1 < 10) fileName = String.format(folderName1 + "/TEST_FALSE_0%d" + fileExtension, i + 1);
+            else fileName = String.format(folderName1 + "/TEST_FALSE_%d" + fileExtension, i + 1);
+
+            Shelf shelf = new Shelf(fileName);
+
+            assertFalse(commonGoalCardStrat.checkPattern(shelf));
+        }
+
+        for (int i = 0; i < numberOfTests; i++) {
+            String fileName;
+            if (i + 1 < 10) fileName = String.format(folderName1 + "TEST_TRUE_0%d" + fileExtension, i + 1);
+            else fileName = String.format(folderName1 + "TEST_TRUE_%d" + fileExtension, i + 1);
+
+            Shelf shelf = new Shelf(fileName);
+
+            assertTrue(commonGoalCardStrat.checkPattern(shelf));
+        }
+    }
+
+
+}
