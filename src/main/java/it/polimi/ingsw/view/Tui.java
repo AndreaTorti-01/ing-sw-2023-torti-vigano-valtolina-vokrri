725a14ff7232fbc772c32b6c1fe12b4a0e5aeeff
Diego <diego3.vigano@mail.polimi.it>
2023-04-17 13:47:51 +0200

    Started Tui coding, added useful methods and ansi colors

--- src/main/java/it/polimi/ingsw/view/Tui.java
+++ src/main/java/it/polimi/ingsw/view/Tui.java
@@ -3,8 +3,18 @@ package it.polimi.ingsw.view;
 import it.polimi.ingsw.model.GameView;
 import it.polimi.ingsw.utils.Observable;
 import it.polimi.ingsw.utils.Observer;
+import java.util.Scanner;
 
 public class Tui extends Observable<String> implements Observer<GameView, String>, Runnable {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     @Override
     public void run() {
         // Ask the names of players
@@ -14,6 +24,70 @@ public class Tui extends Observable<String> implements Observer<GameView, String
         }
     }
 
+    private void printBoard() {
+        // Print the board
+    }
+    private void printShelf() {
+        // Print the shelf
+    }
+    private void printGameStatus(){
+        // Print the game status, including the main board and the shelves
+    }
+
+    private void printLoadingScreen() {
+        // Print the loading screen, asking for player names etc...
+    }
+
+    private void printEndScreen() {
+        // Print the end screen, showing the winner
+    }
+
+    private void printError(String error) {
+        // Print an error message
+        System.out.println(ANSI_RED + error + ANSI_RESET);
+    }
+
+    private String askPlayerName() {
+        // Ask the name of the player
+        Scanner in = new Scanner(System.in);
+        System.out.println("  >>  Type the new player name:");
+        return in.nextLine();
+    }
+
+    private int[] askPlayerChoice() {
+        // Ask the player to choose a card
+        int[] coordinates = new int[2];
+        Scanner in = new Scanner(System.in);
+        System.out.println("  >>  Type the row number:");
+        coordinates[0] = Integer.parseInt(in.next());
+        System.out.println("  >>  Type the column number:");
+        coordinates[1] = Integer.parseInt(in.next());
+        return coordinates;
+    }
+
+    private int askPlayerInsertion() {
+        // Ask the player to insert a card
+        Scanner in = new Scanner(System.in);
+        System.out.println("  >>  Type a column number:");
+        return Integer.parseInt(in.next());
+    }
+
+    private boolean askBoolean(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("  >>  (y/n)");
+
+        while (true) {
+            String input = in.nextLine();
+            char c = Character.toLowerCase(input.charAt(0));
+            if (c == 'y') {
+                return true;
+            } else if (c == 'n') {
+                return false;
+            } else {
+                System.out.println("  >>  Please enter a valid input  (y/n)  <<");
+            }
+        }
+    }
     @Override
     public void update(GameView o, String arg) {
 
