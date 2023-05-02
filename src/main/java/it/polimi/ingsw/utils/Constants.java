ed8e201ccfbc5b3c28642b4bed8d0556e9dd22a2
Diego <diego3.vigano@mail.polimi.it>
2023-05-02 13:44:35 +0200

    merge to pull

--- src/main/java/it/polimi/ingsw/utils/Constants.java
+++ src/main/java/it/polimi/ingsw/utils/Constants.java
@@ -13,4 +13,13 @@ public class Constants {
     public static final int boardSize = 9;
     public static final String serverIpAddress = "127.0.0.1";
     public static final int serverPort = 8888;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 }
--- src/main/java/it/polimi/ingsw/view/Tui.java
+++ src/main/java/it/polimi/ingsw/view/Tui.java
@@ -12,20 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static it.polimi.ingsw.utils.Constants.numberOfColumns;
-import static it.polimi.ingsw.utils.Constants.numberOfRows;
+import static it.polimi.ingsw.utils.Constants.*;
 
 public class Tui extends Observable implements RunnableView {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     String playerName;
     Player me = null;
 
