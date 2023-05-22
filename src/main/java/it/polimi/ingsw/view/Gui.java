08b18c61c9788fd4414ee9f01e39984c10245f0e
vcristiano2000 <126269576+vcristiano2000@users.noreply.github.com>
2023-05-22 12:41:45 +0200

    GUi-scena1

--- src/main/java/it/polimi/ingsw/view/Gui.java
+++ src/main/java/it/polimi/ingsw/view/Gui.java
@@ -5,28 +5,10 @@ import javafx.stage.Stage;
 
 import javax.swing.*;
 
-public class Gui extends Application {
-    private final JFrame frame;
-    private final JPanel panel;
-    private final JButton button;
-    private final JLabel label;
+public abstract class Gui extends Application {
 
     public Gui() {
-        frame = new JFrame("Frame");
-        panel = new JPanel();
-        button = new JButton("Button");
-        label = new JLabel("Label");
 
-        panel.add(button);
-        panel.add(label);
-        frame.add(panel);
-        frame.setSize(300, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException();
-    }
 }
