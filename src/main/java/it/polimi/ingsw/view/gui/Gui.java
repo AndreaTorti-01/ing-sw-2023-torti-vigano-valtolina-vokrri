f7daa4e2629c7c87bdd074232510b4db1f4d105e
Diego <diego3.vigano@mail.polimi.it>
2023-05-23 15:20:42 +0200

    mini gui push

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -1,12 +1,21 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
-import javafx.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public abstract class Gui extends Application {
+import java.util.Objects;
 
-    public Gui() {
+public class Gui extends Application {
 
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scena1.fxml")));
+        primaryStage.setTitle("My Shelfie");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+    public Gui() {}
 
 }
---
+++ src/main/java/it/polimi/ingsw/view/gui/GuiTestRunner.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.stage.Stage;
+
+public class GuiTestRunner {
+    public static void main(String[] args) throws Exception {
+        Gui gui = new Gui();
+
+        gui.start(new Stage());
+    }
+}
