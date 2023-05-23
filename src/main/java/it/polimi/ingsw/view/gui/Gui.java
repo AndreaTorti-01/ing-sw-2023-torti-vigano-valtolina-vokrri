5e7aea7b6c268abbb0c0691c0b04c7f8ce0fe975
DiegosDigoss <126207707+DiegosDigoss@users.noreply.github.com>
2023-05-23 23:20:49 +0200

    setup-push for cri

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -1,18 +1,16 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class Gui extends Application {
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scena1.fxml")));
-        primaryStage.setTitle("My Shelfie");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
     }
 }
