d0ea9d808a30e4d4e2810de67e64cceeec9cf97e
Diego <diego3.vigano@mail.polimi.it>
2023-05-26 11:15:53 +0200

    Push for cristiano, grazie burro la gui ora va e ti porto i crackers martedì e il cavo jack piu bello che ho :D

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -1,16 +1,30 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Gui extends Application {
+    Stage mainStage;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        mainStage = primaryStage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/graphicalResources/fxml/scena1.fxml"));
+
+        Parent root = loader.load();
+
+        mainStage.setTitle("MyShelfie");
+        mainStage.setScene(new javafx.scene.Scene(root));
+        mainStage.show();
 
     }
 }
