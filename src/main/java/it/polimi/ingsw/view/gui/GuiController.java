88e7a9ac911e60f305cf308317b429a9cd8fea54
Diego <diego3.vigano@mail.polimi.it>
2023-05-26 12:36:05 +0200

    fxml fixed

--- src/main/java/it/polimi/ingsw/view/gui/GuiController.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiController.java
@@ -6,4 +6,9 @@ import javafx.scene.image.ImageView;
 public class GuiController {
     public ImageView nickname_label;
     public TextField insert_nickname;
+
+    public String getNickname() {
+        return insert_nickname.getText();
+    }
+
 }
