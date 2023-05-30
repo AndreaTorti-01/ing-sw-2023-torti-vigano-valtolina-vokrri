4a459b0acd3441a96d385c3d440a2e51626dacef
DiegosDigoss <126207707+DiegosDigoss@users.noreply.github.com>
2023-05-30 18:56:53 +0200

    first controller is ok!

--- src/main/java/it/polimi/ingsw/view/gui/controllers/WelcomeScreenController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/WelcomeScreenController.java
@@ -1,11 +1,14 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -20,13 +23,46 @@ public class WelcomeScreenController implements Initializable {
     @FXML
     public ImageView nickname_label;
     @FXML
-    public TextField insert_nickname;
+    public TextField textField;
+    @FXML
+    public Button enterButton;
+    @FXML
+    public Button button4;
+    @FXML
+    public Button button3;
+    @FXML
+    public Button button2;
+    @FXML
+    public Label askerLabel;
+    private boolean insertedName = false;
+    private int numberOfPlayers;
+    private String PlayerName;
     private Stage stage;
     private Scene scene;
     private Parent root;
     @FXML
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        enterButton.setText("Enter");
+        askerLabel.setText("Insert your nickname");
+
+        button2.setVisible(false);
+        button3.setVisible(false);
+        button4.setVisible(false);
+
+        button2.setDisable(true);
+        button3.setDisable(true);
+        button4.setDisable(true);
+
+        this.numberOfPlayers = 0;
+        this.PlayerName = "";
+    }
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public String getNickname() {
+        return PlayerName;
     }
     public Parent loadResource(String fxmlName, Parent root) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath + fxmlName));
@@ -37,14 +73,45 @@ public class WelcomeScreenController implements Initializable {
         }
         return root;
     }
-
-    public void changeScene(javafx.event.ActionEvent actionEvent) throws IOException {
-        root = loadResource("PlayingScreen.fxml", root);
-        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    public void changeScene(String sceneName){
+        root = loadResource(sceneName, root);
+        stage = (Stage) nickname_label.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
+    public void ButtonAction(ActionEvent actionEvent) {
+        if(!textField.getText().isEmpty() && !insertedName){
+            this.PlayerName = textField.getText();
+            insertedName = true;
+            textField.clear();
+            askerLabel.setText("How many players?");
 
+            textField.setVisible(false);
+            textField.setDisable(true);
+
+            enterButton.setVisible(false);
+            button2.setVisible(true);
+            button3.setVisible(true);
+            button4.setVisible(true);
+
+            enterButton.setDisable(true);
+            button2.setDisable(false);
+            button3.setDisable(false);
+            button4.setDisable(false);
+        }
+    }
+
+    public void Player2(ActionEvent actionEvent) {
+        this.numberOfPlayers = 2;
+    }
+
+    public void Player3(ActionEvent actionEvent) {
+        this.numberOfPlayers = 3;
+    }
+
+    public void Player4(ActionEvent actionEvent) {
+        this.numberOfPlayers = 4;
+    }
 }
