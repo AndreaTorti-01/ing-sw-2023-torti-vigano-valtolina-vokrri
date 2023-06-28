be966234812881bea8a48ee26ec073e1f788eb08
vcristiano2000 <126269576+vcristiano2000@users.noreply.github.com>
2023-06-28 16:50:30 +0200

    Add CommonTest

--- src/test/java/it/polimi/ingsw/utils/CommonTest.java
+++ src/test/java/it/polimi/ingsw/utils/CommonTest.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.utils;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.ItemCards.ItemCard;
 import it.polimi.ingsw.model.ItemCards.ItemType;
 import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.commonGoalCards.CommonGoalCardType;
+import it.polimi.ingsw.network.serializable.GameViewMsg;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +17,16 @@ class CommonTest {
 
     @Test
     void isTakeable() {
-
+        Game g = new Game();
+        g.initModel(2);
+        g.addPlayer("diego");
+        g.addPlayer("cristiano");
+        List<List<Integer>> pickedCards = new ArrayList<>();
+        List<Integer> cords = new ArrayList<>();
+        cords.add(1);
+        cords.add(0);
+        pickedCards.add(cords);
+        Common.isTakeable(new GameViewMsg(g),2,3, pickedCards );
     }
 
     @Test
@@ -40,23 +55,45 @@ class CommonTest {
 
     @Test
     void getHeads() {
+        Shelf shelf = new Shelf().getCopy();
+        ItemCard itemCard = new ItemCard(ItemType.TROPHIES,2);
+        ItemCard itemCard1 = new ItemCard(ItemType.CATS,2);
+        for(int row=0; row < Constants.numberOfRows; row++){
+            for(int col=0; col< Constants.numberOfColumns; col++) {
+               shelf.setCardAt(row,col,new ItemCard(ItemType.TROPHIES,2));
+            }
+        }
+        assertEquals(30,1+Common.headLiminate(shelf,shelf.getCardAt(3,3)));
+
     }
 
     @Test
     void getCommonGoalCardPath() {
+        for (CommonGoalCardType type : CommonGoalCardType.values())
+            assertDoesNotThrow(()->Common.getCommonGoalCardPath(type));
+
+
     }
 
     @Test
     void getCommonGoalCardPointPath() {
+        assertDoesNotThrow(()->Common.getCommonGoalCardPointPath(0));
+        assertDoesNotThrow(()->Common.getCommonGoalCardPointPath(2));
     }
 
     @Test
     void getPersonalGoalCardPath() {
+        assertDoesNotThrow(()->Common.getPersonalGoalCardPath(8));
 
     }
 
     @Test
     void getTilePath() {
-
+    for (int i=0; i<3; i++){
+        for(ItemType item: ItemType.values()){
+            ItemCard ic = new ItemCard(item,i);
+            assertDoesNotThrow(()->Common.getTilePath(ic));
+        }
+    }
     }
 }
